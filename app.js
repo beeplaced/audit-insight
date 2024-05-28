9589e4b3b7a0d74b7cb2d8d344f870c5a186e885
beeplaced <you@example.com>
2024-05-28 17:32:33 +0200

    w2

--- app.js
+++ app.js
@@ -13,18 +13,17 @@ app.use('/client', express.static(path.join(__dirname, 'client')));
 //app.use('/custom-elements', express.static(__dirname + '/node_modules/custom-elements/components.js'));
 const winston = require('winston');
 
-// Create a new winston logger instance
 const logger = winston.createLogger({
-    level: 'info', // Set the log level
+    level: 'info',
     format: winston.format.combine(
-        winston.format.timestamp(),  // Add timestamp to each log
+        winston.format.timestamp(),
         winston.format.printf(({ timestamp, level, message }) => {
             return `${timestamp} [${level.toUpperCase()}]: ${message}`;
         })
     ),
     transports: [
-        new winston.transports.File({ filename: 'app.log' }), // Log to a file
-        new winston.transports.Console() // Optionally log to the console
+        new winston.transports.Console(), // Console transport
+        new winston.transports.File({ filename: 'app.log' }) // File transport
     ]
 });
 
